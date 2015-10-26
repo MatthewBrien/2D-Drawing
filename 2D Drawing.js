7ed4b79f0639be8a5e12d4d1d499322258f8b9c4
Zachary Wartell <zwartell@uncc.edu>
2015-10-26 10:25:26 -0400

    -[BUGS] -[MISSING FILE]

--- 2D Drawing.js
+++ 2D Drawing.js
@@ -11,15 +11,26 @@
  * 
  *****/
 
-var draw_mode = {DrawLines: 0, DrawLines2: 4, DrawTriangles: 1, ClearScreen: 2, None: 3};
+// 'draw_mode' are names of the different user interaction modes.
+// \todo Student Note: others are probably needed...
+var draw_mode = {DrawLines: 0, DrawTriangles: 1, ClearScreen: 2, None: 3};
 
-var curr_draw_mode = draw_mode.None;
+// 'curr_draw_mode' tracks the active user interaction mode
+var curr_draw_mode = draw_mode.DrawLines;
 
+// GL array buffers for points, lines, and triangles
+// \todo Student Note: need similar buffers for other draw modes...
 var vBuffer_Pnt, vBuffer_Line;
 
+// Array's storing 2D vertex coordinates of points, lines, triangles, etc.
+// Each array element is an array of size 2 storing the x,y coordinate.
+// \todo Student Note: need similar arrays for other draw modes...
 var points = [], line_verts = [], tri_verts = [];
 
+// count number of points clicked for new line
 var num_pts_line = 0;
+
+// \todo need similar counters for other draw modes...
 
 
 /*****
@@ -29,23 +40,29 @@ var num_pts_line = 0;
  *****/
 function main() {
     
+    //math2d_test();
+    
     /**
      **      Initialize WebGL Components
      **/
     
+    // Retrieve <canvas> element
     var canvas = document.getElementById('webgl');
 
+    // Get the rendering context for WebGL
     var gl = getWebGLContext(canvas);
     if (!gl) {
         console.log('Failed to get the rendering context for WebGL');
         return;
     }
 
+    // Initialize shaders
     if (!initShadersFromID(gl, "vertex-shader", "fragment-shader")) {
         console.log('Failed to intialize shaders.');
         return;
     }
 
+    // create GL buffer objects
     vBuffer_Pnt = gl.createBuffer();
     if (!vBuffer_Pnt) {
         console.log('Failed to create the buffer object');
@@ -58,11 +75,21 @@ function main() {
         return -1;
     }
 
+    var skeleton=true;
+    if(skeleton)
+    {
+        document.getElementById("App_Title").innerHTML += "-Skeleton";
+    }
 
+    // \todo create buffers for triangles and quads...
+
+    // Specify the color for clearing <canvas>
     gl.clearColor(0, 0, 0, 1);
 
+    // Clear <canvas>
     gl.clear(gl.COLOR_BUFFER_BIT);
 
+    // get GL shader variable locations
     var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
     if (a_Position < 0) {
         console.log('Failed to get the storage location of a_Position');
@@ -81,6 +108,7 @@ function main() {
      **  Student Note: the WebGL book uses an older syntax. The newer syntax, explicitly calling addEventListener, is preferred.
      **  See https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
      **/
+    // set event handlers buttons
     document.getElementById("LineButton").addEventListener(
             "click",
             function () {
@@ -97,6 +125,7 @@ function main() {
             "click",
             function () {
                 curr_draw_mode = draw_mode.ClearScreen;
+                // clear the vertex arrays
                 while (points.length > 0)
                     points.pop();
                 while (line_verts.length > 0)
@@ -109,7 +138,9 @@ function main() {
                 curr_draw_mode = draw_mode.DrawLines;
             });
             
+    //\todo add event handlers for other buttons as required....            
 
+    // set event handlers for color sliders
     /* \todo right now these just output to the console, code needs to be modified... */
     document.getElementById("RedRange").addEventListener(
             "input",
@@ -127,10 +158,13 @@ function main() {
                 console.log("BlueRange:" + document.getElementById("BlueRange").value);
             });                        
             
+    // init sliders 
+    // \todo this code needs to be modified ...
     document.getElementById("RedRange").value = 0;
     document.getElementById("GreenRange").value = 100;
     document.getElementById("BlueRange").value = 0;
             
+    // Register function (event handler) to be called on a mouse press
     canvas.addEventListener(
             "mousedown",
             function (ev) {
@@ -159,21 +193,28 @@ function handleMouseDown(ev, gl, canvas, a_Position, u_FragColor) {
     var y = ev.clientY; // y coordinate of a mouse pointer
     var rect = ev.target.getBoundingClientRect();
     
+    // Student Note: 'ev' is a MouseEvent (see https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent)
     
+    // convert from canvas mouse coordinates to GL normalized device coordinates
     x = ((x - rect.left) - canvas.width / 2) / (canvas.width / 2);
     y = (canvas.height / 2 - (y - rect.top)) / (canvas.height / 2);
 
     if (curr_draw_mode !== draw_mode.None) {
+        // add clicked point to 'points'
         points.push([x, y]);
     }
 
+    // perform active drawing operation
     switch (curr_draw_mode) {
         case draw_mode.DrawLines:
+            // in line drawing mode, so draw lines
             if (num_pts_line < 1) {			
+                // gathering points of new line segment, so collect points
                 line_verts.push([x, y]);
                 num_pts_line++;
             }
             else {						
+                // got final point of new line, so update the primitive arrays
                 line_verts.push([x, y]);
                 num_pts_line = 0;
                 points.length = 0;
@@ -193,20 +234,29 @@ function handleMouseDown(ev, gl, canvas, a_Position, u_FragColor) {
  */
 function drawObjects(gl, a_Position, u_FragColor) {
 
+    // Clear <canvas>
     gl.clear(gl.COLOR_BUFFER_BIT);
 
+    // draw lines
     if (line_verts.length) {	
+        // enable the line vertex
         gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer_Line);
+        // set vertex data into buffer (inefficient)
         gl.bufferData(gl.ARRAY_BUFFER, flatten(line_verts), gl.STATIC_DRAW);
+        // share location with shader
         gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(a_Position);
 
         gl.uniform4f(u_FragColor, 0.0, 1.0, 0.0, 1.0);
+        // draw the lines
         gl.drawArrays(gl.LINES, 0, line_verts.length );
     }
 
+   // \todo draw triangles
    
+   // \todo draw quads
     
+    // draw primitive creation vertices 
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer_Pnt);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
     gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
---
+++ math2D.js
@@ -0,0 +1,246 @@
+/**
+ * @author Zachary Wartell, ...
+ * 
+ * math2D.js is a set of 2D geometry related math functions and classes.
+ * 
+ * Students are given a initial set of classes and functions are expected to extend these and add
+ * additional functions to this file.
+ * 
+ */
+
+/**
+ * Constructor of Mat2, a 2x2 matrix 
+ * 
+ * For efficiency we use a Typed Array.  Elements are stored in 'column major' layout, i.e.
+ * for matrix M with math convention M_rc
+ *    this.array = [ M_00, M_10,    // first column
+ *                   M_01, M_11 ];  // second column
+ *                   
+ *                   
+ * column major order is consistent with OpenGL and GLSL
+ *                   
+ * @param {null}                  
+ * @returns {Mat2}
+ */
+var Mat2 = function()
+{
+    this.array = new Float32Array(4);
+    this.array.set([1.0, 0.0, 
+                    0.0, 1.0]);
+};
+
+/**
+ * 'get' returns element in column c, row r of this Mat2
+ * @param {Number} c - column
+ * @param {Number} r - row
+ * @returns {Number}
+ */
+Mat2.prototype.get = function (c, r)
+{
+    return this.array[c*2+r];
+};
+
+/**
+ * 'set' sets element at column c, row r to value 'val'.
+ * @param {Number} c - column
+ * @param {Number} r - row
+ * @param {Number} val - value
+ * @returns {Number}
+ */
+Mat2.prototype.set = function (c, r, val)
+{
+    this.array[c*2+r] = val;
+};
+
+/**
+ * 'det' return the determinant of this Mat2
+ * @returns {Number}
+ */
+Mat2.prototype.det = function ()
+{
+    return this.array[0] * this.array[3] - this.array[1] * this.array[2];
+};
+
+/**
+ * Constructor of Vec2. Vec2 is is used to represent coordinates of geometric points or vectors. 
+ * 
+ * @param {null | Vec2 | [Number, Number]}
+ */
+var Vec2 = function ()
+{
+    if (arguments.length === 0)
+    {// no arguements, so initial to 0's
+        this.array = new Float32Array(2);
+        this.array.set([0.0, 0.0]);
+    }
+    else if (arguments.length === 1)
+    {// 1 argument, ...
+        if (arguments[0] instanceof Vec2)
+        {// argument is Vec2, so copy it
+            this.array = new Float32Array(arguments[0].array);
+        }
+        else if (arguments[0] instanceof Array)
+        {// argument is Array, so copy it
+            this.array = new Float32Array(arguments[0]);
+        }
+    }
+};
+
+/**
+ *  Vec2 - provide alternate syntax for setting/getting x and y coordinates (see math2d_test for examples).
+ */
+var v = Vec2.prototype;
+Object.defineProperties(Vec2.prototype,
+        {
+            "x": {get: function () {
+                    return this.array[0];
+                },
+                set: function (v) {
+                    this.array[0] = v;
+                }},
+            "y": {get: function () {
+                    return this.array[1];
+                },
+                set: function (v) {
+                    this.array[1] = v;
+                }}
+        }
+);
+
+
+/**
+ * Add Vec2 'v' to this Vec2
+ * @param {Vec2} v    
+ */
+Vec2.prototype.add = function (v)
+{
+    this.array.set([this.array[0] + v.array[0], this.array[1] + v.array[1]]);
+};
+
+/**
+ * Subtract Vec2 'v' from this Vec2
+ * @param {Vec2} v    
+ */
+Vec2.prototype.sub = function (v)
+{
+    /*
+     * \todo needs to be implemented
+     */    
+};
+
+/**
+ * Treat this Vec2 as a column matrix and multiply it by Mat2 'm' to it's left, i.e.
+ * 
+ * v = m * v
+ * 
+ * @param {Mat2} m    
+ */
+Vec2.prototype.multiply = function (m)
+{
+     this.array.set([this.array[0]*m.array[0] + this.array[1]*m.array[2],
+                     this.array[0]*m.array[1] + this.array[1]*m.array[3] ]);
+};
+
+/**
+ * Treat this Vec2 as a row matrix and multiply it by Mat2 'm' to it's right, i.e.
+ * 
+ * v = v * m
+ * 
+ * @param {Mat2} m
+ */
+Vec2.prototype.rightMultiply = function (m)
+{
+     this.array.set([this.array[0]*m.array[0] + this.array[1]*m.array[2],
+                     this.array[0]*m.array[1] + this.array[1]*m.array[3] ]);
+};
+
+/**
+ * Return the dot product of this Vec2 with Vec2 'v'
+ * @param {Vec2} v    
+ * @return {Number}
+ */
+Vec2.prototype.dot = function (v)
+{
+    /*
+     * \todo needs to be implemented
+     */
+    return 0;
+};
+
+/**
+ * Return the magnitude (i.e. length) of of this Vec2 
+ * @return {Number}
+ */
+Vec2.prototype.mag = function ()
+{
+    /*
+     * \todo needs to be implemented
+     */
+    return 0;
+};
+
+/**
+ * Compute the barycentric coordinate of point 'p' with respect to barycentric coordinate system
+ * defined by points p0,p1,p2.
+ * 
+ * @param {Vec2} p0 - first point of barycentric coordinate system
+ * @param {Vec2} p1 - second point of barycentric coordinate system
+ * @param {Vec2} p2 - third point of barycentric coordinate system
+ * @param {Vec2} p  - point to compute the barycentric coordinate of
+ * @returns {[Number, Number, Number]} - array with barycentric coordinates of 'p'
+ */
+function barycentric (p0, p1, p2, p)
+{
+    /*
+     * \todo needs to be implemented
+     */    
+    return [0,0,0];
+}
+
+/**
+ * Compute distance between point 'p' and the line through points 'p0' and 'p1'
+ * @param {Vec2} p0 - first point on line
+ * @param {Vec2} p1 - second point on line
+ * @param {Vec2} p  - point for which we are computing distance
+ * @returns {undefined}
+ */
+function pointLineDist(p0, p1, p)
+{
+     /*
+     * \todo needs to be implemented
+     */    
+    return 0;
+}
+
+/**
+ * This contains misc. code for testing the functions in this file.
+ * 
+ * Students can optionally use this function for testing their code...
+ * @returns {undefined}
+ */
+function math2d_test()
+{
+    var M1 = new Mat2();
+    var v0 = new Vec2(), v1 = new Vec2([5.0,5.0]), v2, 
+            vx = new Vec2([1.0,0.0]),
+            vy = new Vec2([0.0,1.0]);
+    
+    var rad = 45 * Math.PI/180;
+    M1.set(0,0, Math.cos(rad)); M1.set(1,0, -Math.sin(rad)); 
+    M1.set(0,1, Math.sin(rad)); M1.set(1,1, Math.cos(rad));
+    
+       
+    v0.x = 1.0;
+    v0.y = 2.0;
+    v0.y += 1.0;
+    v2 = new Vec2(v0);
+    v2.add(v1);
+    
+    vx.multiply(M1);       
+    vy.multiply(M1);       
+    
+    console.log (JSON.stringify(M1));
+    console.log (JSON.stringify(v2));
+    console.log (v0.dot(v1));
+    console.log (v0.mag());
+}
